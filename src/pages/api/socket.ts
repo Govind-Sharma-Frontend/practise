e6079e690affce634f8813caccd6ddee0ae9fd0e
Govind Sharma <govind.sharma@sqaurebits.com>
2024-12-02 13:05:30 +0530

    fix: live api issue

--- src/pages/api/socket.ts
+++ src/pages/api/socket.ts
@@ -30,8 +30,14 @@ export default function SocketHandler(_req: NextApiRequest, res: NextApiResponse
 
   res.setHeader('Cache-Control', 'no-store');
   console.log("Starting Socket.IO server on port:", 3000 + 1)
-  const io = new Server({ path: "/api/socket", addTrailingSlash: false, cors: { origin: "*" } }).listen(3000 + 1)
-  // const io = new Server({ path: "/api/socket", addTrailingSlash: false, cors: { origin: "*" } }).listen(3000);
+  // const io = new Server({ path: "/api/socket", addTrailingSlash: false, cors: { origin: "*" } }).listen(3000 + 1)
+  const io = new Server(res.socket.server, {
+    path: "/api/socket",
+    cors: {
+      origin: "*", // Adjust as needed for security
+      methods: ["GET", "POST"],
+    },
+  });
   io.on("connect", socket => {
     const _socket = socket
     console.log("socket connect", socket.id)
