6a7b39439b99739cbbda4530dd5c9bf968e54b04
Govind Sharma <govind.sharma@sqaurebits.com>
2024-12-02 12:12:45 +0530

    fix: URL

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -31,7 +31,7 @@ export default function Chat() {
     socket.on("connect_error", async (err) => {
       console.log(`connect_error due to ${err.message}`);
       try {
-        const res = await fetch("http://localhost:3000/api/socket"); // Adjust the URL to match the Socket.IO server's actual URL.
+        const res = await fetch("https://govind-practise.vercel.app/api/socket"); // Adjust the URL to match the Socket.IO server's actual URL.
         console.log("res Socket", await res.json());
       } catch (fetchError) {
         console.log("Error fetching socket status:", fetchError);
